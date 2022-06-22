c733162fe68b79b38ab607474e077433a5122652
colo <colo>
2022-06-22 18:05:39 -0300

    entrego 2da vez

--- main.js
+++ main.js
@@ -6,45 +6,127 @@ let dict = [
   'fuego',
   'huevo',
   'color',
-  'siete',
+  'casas',
   'arbol',
   'nueve',
-  'mirko'
+  'zorro',
+  'remar',
+  'locro',
+  'jamon',
+  'feliz',
+  'cinco',
+  'tonto',
+  'cuero',
+  'volar',
+  'vuelo',
+  'avion',
+  'genio',
+  'pibes'
 ];
 
-let randomValue = Math.trunc(Math.random() * dict.length);
+// let randomValue = Math.trunc(Math.random() * dict.length);
+//
+// console.log(dict[randomValue]);
+//
+// for (let a = 0; a < 5; a++) {
+//   let div = document.createElement("div");
+//   div.textContent = a;
+//   if (a % 2 == 0)
+//   div.classList.add("yellow");
+//   document.body.append(div);
+// }
+//
+// //let p1 = document.getElementsById('p1');
+// //p1[0].textContent = 'gatos';
+//
+// //console.log(dict)
+// let handleClick = e => {
+//   let cont = 0;
+//   // console.log(e.target);
+//   // alert("HOLA");
+//   // console.log(document.forms);
+//   for (let form of document.forms) {
+//     for (let elem of form.elements) {
+//       // console.log(elem);
+//       // console.log(elem.dataset.correcta);
+//       console.log(elem.dataset.correcta);
+//       if (elem.checked && elem.dataset.correcta == "true") {
+//         cont++;
+//         // console.log('en el if');
+//       }
+//     }
+//   }
+//   console.log(cont);
+// };
 
-console.log(randomValue);
-console.log(dict[randomValue]);
+let boton = document.getElementById("boton");
+let palabra = document.getElementById("palabra");
+let respuesta = document.getElementById("un_div");
+let i = Math.trunc(Math.random() * 10);
+let correcta = dict[i];
+let intentos = 0;
+console.log(correcta);
 
-for (let a = 0; a < 5; a++) {
-  let div = document.createElement("div");
-  div.textContent = a;
-  if (a % 2 == 0)
-  div.classList.add("yellow");
-  document.body.append(div);
+function ganar() {
+  console.log("Bien genio ganaste!");
 }
 
+function perder() {
+  console.log("Lo siento capo perdiste");
+}
 
-//let p1 = document.getElementsById('p1');
-//p1[0].textContent = 'gatos';
+function comprobar() {
+  let a = 0;
+  let com = palabra.value.toLowerCase();
+  let div = document.createElement("div");
+  respuesta.append(div);
+  console.log(com);
 
-//console.log(dict)
-let handleClick = e => {
-  let cont = 0;
-  // console.log(e.target);
-  // alert("HOLA");
-  // console.log(document.forms);
-  for (let form of document.forms) {
-    for (let elem of form.elements) {
-      // console.log(elem);
-      // console.log(elem.dataset.correcta);
-      console.log(elem.dataset.correcta);
-      if (elem.checked && elem.dataset.correcta == "true") {
-        cont++;
-        // console.log('en el if');
-      }
-    }
+  for (let j of com) {
+  div.setAttribute("id", a);
+  if (j == correcta[a]) {
+    let letra = document.getElementById(a);
+    let letras = document.createElement("span");
+    letra.append(letras);
+    letra.childNodes[a].innerHTML = j;
+    letra.childNodes[a].classList.add("verde");
   }
-  console.log(cont);
-};
+  else if (correcta.includes(j)) {
+    let letras = document.createElement("span");
+    let letra = document.getElementById(a);
+    letra.append(letras);
+    letra.childNodes[a].innerHTML = j;
+    letra.childNodes[a].classList.add("yellow");
+  }
+  else {
+    let letras = document.createElement("span");
+    let letra = document.getElementById(a);
+    letra.append(letras);
+    letra.childNodes[a].innerHTML = j;
+    letra.childNodes[a].classList.add("gris");
+  }
+  a++;
+  if (com == correcta) {
+    ganar();
+  }
+  if (intentos == 6) {
+  perder();
+   }
+ }
+intentos++;
+if (intentos == 6) {
+  perder();
+  }
+}
+function recargar() {
+  location.reload();
+}
+function amarillo() {
+  console.log("Letra cerca");;
+}
+function verde() {
+  console.log("Letra Correcta");
+}
+function gris() {
+  console.log("Letra Incorrecta");
+}
